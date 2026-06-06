5bd1936bc4a967f20f306d499f5537007848b997
Tanq-1 <tanquiry@gmail.com>
2026-06-06 09:33:09 +0700

    feat: added new imports

--- rapor-backend/schemas/__init__.py
+++ rapor-backend/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .user import UserCreate, UserUpdate, UserOut, TokenOut
-from .grade_table import GradeTableCreate, GradeTableOut
+from .grade_table import GradeTableCreate, GradeTableOut, GradeTableUpdate
 from .grade_component import GradeComponentCreate, GradeComponentOut
 from .student import StudentCreate, StudentOut
 from .score import ScoreCreate, ScoreOut, FinalGradeOut
