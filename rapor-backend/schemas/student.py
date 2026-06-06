eac8e64a3a15cbee46283acfde449e97f6b10918
Tanq-1 <tanquiry@gmail.com>
2026-06-06 11:15:09 +0700

    feat: updated student.py

--- rapor-backend/schemas/student.py
+++ rapor-backend/schemas/student.py
@@ -1,10 +1,20 @@
 from pydantic import BaseModel, ConfigDict
 
+
 class StudentCreate(BaseModel):
     name: str
+    student_number: str | None = None
+
+
+class StudentUpdate(BaseModel):
+    name: str | None = None
+    student_number: str | None = None
+
 
 class StudentOut(BaseModel):
     id: int
     name: str
+    student_number: str | None = None
+    grade_table_id: int
 
     model_config = ConfigDict(from_attributes=True)
